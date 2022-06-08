41efb968eb29780d07849113e9864f3536dbc9ad
Sebastian Moreno <sebastian.moreno.contacto@gmail.com>
2022-06-07 20:50:27 -0500

    Resolve fetch API and render to the DOM

--- src/index.js
+++ src/index.js
@@ -1,15 +1,29 @@
-const $app = document.getElementById('app');
-const $observe = document.getElementById('observe');
-const API = 'https://api.escuelajs.co/api/v1/products';
+const $app = document.getElementById("app");
+const $observe = document.getElementById("observe");
+const API = " https://api.escuelajs.co/api/v1/products?offset=4&limit=10";
+
 
 const getData = api => {
+  
   fetch(api)
     .then(response => response.json())
     .then(response => {
       let products = response;
-      let output = products.map(product => {
+      let output = '';
+      
+      products.map(product => { output = output +
         // template
+        `
+        <article class="Card">
+          <img src=${product.images} />
+          <h2>
+            ${product.title}
+            <small>${product.price}</small>
+          </h2>
+        </article>
+        `
       });
+
       let newItem = document.createElement('section');
       newItem.classList.add('Item');
       newItem.innerHTML = output;
@@ -22,10 +36,29 @@ const loadData = () => {
   getData(API);
 }
 
+loadData();
+
+
+
+
+
 const intersectionObserver = new IntersectionObserver(entries => {
   // logic...
+
+
 }, {
   rootMargin: '0px 0px 100% 0px',
 });
 
 intersectionObserver.observe($observe);
+
+
+const savedData = () => {
+
+  localStorage.setItem(
+    'entries',
+    'https://api.escuelajs.co/api/v1/products?offset=4&limit=10'
+    );
+}
+
+savedData();
